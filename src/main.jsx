8c30ddac811630f66ac4a102ffdc611c77641d03
Anish Pradhan <anishpradha5523@gmail.com>
2023-12-23 12:09:54 +0530

    Todo-List

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import { Store } from './app/store.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={Store}>
     <App />
-  </Provider>,
 )
